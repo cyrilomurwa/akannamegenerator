656ef0b9030292b3e9995bef1c25fcc1c4a1e8f5
Cyril Ondieki Omurwa <COMURWA@safaricom.co.ke>
2021-12-22 06:17:51 +0300

    update script.js file to output from prompt and not a label

--- js/script.js
+++ js/script.js
@@ -77,10 +77,6 @@ function generateAkanName () {
   
     if (myGenderValue == "male" && validM && validD) {
         alert("You were born on a " + daysOfWeek[index] + " and your Akan name is " + maleAkanNames[index]);
-     // document.getElementById('result').textContent = "You were born on a " + daysOfWeek[index] + " and your Akan name is " + maleAkanNames[index];
-     // document.getElementById('display-name').textContent = "Here is your Akan name: ";
-     // document.getElementById('result').style.fontSize = "18px";
-     // document.querySelector('h1').textContent = "Your Akan Name is :" + maleAkanNames[index];
       return false;
     } else if (myGenderValue == "female" && validM && validD) {
       alert("You were born on a " + daysOfWeek[index] + " , your Akan name is " + femaleAkanNames[index]);
